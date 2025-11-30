11f2dddded524d8b3706e4993e85ca7f452349e4
user <zaksroman@gmail.com>
2025-12-01 02:50:30 +0500

    add build

--- src/const.ts
+++
@@ -1,1 +0,0 @@
-export const reownProjectId = import.meta.env.VITE_REOWN_PROJECT_ID
--- src/hooks/helpers/useTrimAddress.ts
+++
@@ -1,3 +0,0 @@
-
-export const useTrimAddress =  (address: string | undefined| null, leftLength = 6, rightLength = 5) =>
-    address ? `${address?.slice(0, leftLength)}....${address?.slice(-rightLength)}` : "";
